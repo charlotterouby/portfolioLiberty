23c5ddc1c9d3dcc1704a92ca8f8958cb5084dfc2
charlotterouby <charlotte.rouby@hotmail.fr>
2016-02-27 22:31:58 +0100

    Responsive layout & mixin alpha-attribute
    _inclusion breakpoint plugin sass
    _mise en place des différences de layout mobile / tablet / desktop
    _ajout d'un mixin alpha-attribute pour créer un attribut de couleur
    transparente
    _réglages layout navigation desktop

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'compass'
 gem 'support-for', '~> 1.0'
 gem 'susy'
+gem 'breakpoint'
--- config.rb
+++ config.rb
@@ -1,7 +1,10 @@
 require 'compass/import-once/activate'
+
 # Require any additional compass plugins here.
 require 'support-for'
 require 'susy'
+require 'breakpoint'
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
