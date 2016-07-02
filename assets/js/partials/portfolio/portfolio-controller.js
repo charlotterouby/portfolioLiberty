68dc978b17332485f99a4ea2dd835f46b01b8004
charlotterouby <charlotte.rouby@hotmail.fr>
2016-07-02 16:20:28 +0200

    ajout des fichiers images dans partial project et modification de portfolio partial pour que les items de la grille soient cliquable et redirige vers project

--- assets/js/partials/portfolio/portfolio-controller.js
+++ assets/js/partials/portfolio/portfolio-controller.js
@@ -1,6 +1,13 @@
 // PortfolioController
-function PortfolioController(Projects) {
+function PortfolioController(Projects, $location) {
     var self = this;
     self.projects = Projects;
+
+    self.goTo = function(projectId){
+        console.log("fonction goTo");
+        var newLocation = "project/"+projectId;
+        $location.path(newLocation);
+    }
+
 }
 myPortfolio.controller('PortfolioController', PortfolioController);
