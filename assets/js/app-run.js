6e4cf1db6d9984f8d673608ce3b42fde2c2fb826
charlotterouby <charlotte.rouby@hotmail.fr>
2016-07-16 16:18:37 +0200

    personnalisation thème couleur angular material, finalisation bouttons réseaux sociaux dans section contact, réglage z-index navbar

--- assets/js/app-run.js
+++ assets/js/app-run.js
@@ -3,25 +3,55 @@ myPortfolio.run(function ($http, Projects) {
     $http
         .get('dist/templates/projects.json')
         .then(function (httpResponse) {
-//            console.log(httpResponse.data);
+            //            console.log(httpResponse.data);
             return httpResponse.data;
         }).then(function (data) {
-//            console.log(data);
+            //            console.log(data);
             angular.forEach(data.projects, function (project) {
                 Projects.add(project);
-//                console.log(project);
+                //                console.log(project);
             });
         });
 });
 
-//Config Theme MaterialDesign
-//
-//myPortfolio.config(function ($mdThemingProvider) {
-//    $mdThemingProvider.theme('charlotte')
-//      .primaryPalette('teal')
-//      .accentPalette('pink');
-//  });
 
-myPortfolio.config(function($mdIconProvider) {
-  $mdIconProvider.fontSet('md', 'material-icons');
+myPortfolio.config(function ($mdIconProvider, $mdThemingProvider) {
+    // Config font Icons
+    $mdIconProvider.fontSet('md', 'material-icons').fontSet('fa', 'font-awesome');
+
+    // Extend the teal theme with different colors
+    var customTealMap = $mdThemingProvider.extendPalette('teal', {
+        '300': '#5bb4b4',
+        '500': '#378c8c',
+        '800': '#024242',
+        'A100': '#8ad9d9'
+    });
+
+    // Extend the pink theme with different colors
+    var peachMap = $mdThemingProvider.extendPalette('pink', {
+        'A100': '#ffc8c8',
+        'A200': '#ffa2a2',
+        'A400': '#ff8080',
+        'A700': '#e95b5b'
+    });
+
+    // Extend the yellow theme with different colors
+    var jauneMap = $mdThemingProvider.extendPalette('yellow', {
+        '300': '#ffe0a2',
+        '500': '#ffd580',
+        '800': '#e9ba5b',
+        'A100': '#ffedc8'
+    });
+
+    // Register the new color palette maps
+    $mdThemingProvider.definePalette('customTeal', customTealMap)
+        .definePalette('peach', peachMap)
+        .definePalette('jaune', jauneMap);
+
+    //Config Theme MaterialDesign
+    $mdThemingProvider.theme('default')
+        .primaryPalette('customTeal')
+        .accentPalette('peach')
+        .warnPalette('jaune');
+
 });
